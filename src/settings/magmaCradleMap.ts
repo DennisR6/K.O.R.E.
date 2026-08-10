21305e1eeabd5e3a92392d99b35ec09b6f82247f
DennisR6 <dennis.zimmerling@osnanet.de>
2026-08-10 21:46:31 +0200

    magma cradle angepasst

--- src/settings/magmaCradleMap.ts
+++ src/settings/magmaCradleMap.ts
@@ -15,5 +15,20 @@ export function createMagmaCradleMap(worldSize: Vector2D): MapDocument {
 	const rect = (x: number, y: number, w: number, h: number) => map.addRectangle({ x: x * scaleX, y: y * scaleY, w: w * scaleX, h: h * scaleY });
 	map.addPlayerSpawn({ teamNr: 0, x: 120 * scaleX, y: 120 * scaleY, w: 200 * scaleX, h: 350 * scaleY, playerCount: 1 });
 	map.addPlayerSpawn({ teamNr: 1, x: 560 * scaleX, y: 120 * scaleY, w: 200 * scaleX, h: 350 * scaleY, playerCount: 1 });
-	return rect(66, 90, 10, 270).addRectangle({ x: 100 * scaleX, y: 50 * scaleY, w: 270 * scaleX, h: 10 * scaleY }).addRectangle({ x: 425 * scaleX, y: 55 * scaleY, w: 270 * scaleX, h: 10 * scaleY }).addRectangle({ x: 100 * scaleX, y: 385 * scaleY, w: 270 * scaleX, h: 10 * scaleY }).addRectangle({ x: 425 * scaleX, y: 385 * scaleY, w: 270 * scaleX, h: 10 * scaleY }).addRectangle({ x: 725 * scaleX, y: 90 * scaleY, w: 10 * scaleX, h: 270 * scaleY }).addRectangle({ x: 400 * scaleX, y: 150 * scaleY, w: 10 * scaleX, h: 150 * scaleY }).addForceZone({ id: "west-vent", x: 300 * scaleX, y: 225 * scaleY, r: 28 * scale, angle: 0, power: 4 }).addForceZone({ id: "east-vent", x: 500 * scaleX, y: 225 * scaleY, r: 28 * scale, angle: 180, power: 4 }).addKillZone({ id: "north-lava", x: 390 * scaleX, y: 35 * scaleY, r: 14 * scale }).addKillZone({ id: "south-lava", x: 390 * scaleX, y: 400 * scaleY, r: 14 * scale }).addKillZone({ id: "north-west-corner-lava", x: 40 * scaleX, y: 40 * scaleY, r: 18 * scale }).addKillZone({ id: "north-east-corner-lava", x: 760 * scaleX, y: 40 * scaleY, r: 18 * scale }).addKillZone({ id: "south-west-corner-lava", x: 40 * scaleX, y: 410 * scaleY, r: 18 * scale }).addKillZone({ id: "south-east-corner-lava", x: 760 * scaleX, y: 410 * scaleY, r: 18 * scale }).buildMapDocument();
+	return rect(66, 90, 10, 270)
+	.addRectangle({ x: 105 * scaleX, y: 55 * scaleY, w: 270 * scaleX, h: 10 * scaleY ,color: "#ff000000"})
+	.addRectangle({ x: 425 * scaleX, y: 55 * scaleY, w: 270 * scaleX, h: 10 * scaleY ,color: "#09ee4e00"})
+	.addRectangle({ x: 105 * scaleX, y: 385 * scaleY, w: 270 * scaleX, h: 10 * scaleY ,color: "#ebdc0600"})
+	.addRectangle({ x: 425 * scaleX, y: 385 * scaleY, w: 270 * scaleX, h: 10 * scaleY ,color: "#071de400"})
+	.addRectangle({ x: 723 * scaleX, y: 90 * scaleY, w: 10 * scaleX, h: 270 * scaleY ,color: "#f700ff00"})
+	.addRectangle({ x: 400 * scaleX, y: 150 * scaleY, w: 10 * scaleX, h: 150 * scaleY ,color: "#07d4f800"})
+	.addForceZone({ id: "west-vent", x: 300 * scaleX, y: 225 * scaleY, r: 28 * scale, angle: 0, power: 4 })
+	.addForceZone({ id: "east-vent", x: 500 * scaleX, y: 225 * scaleY, r: 28 * scale, angle: 180, power: 4 })
+	.addKillZone({ id: "north-lava", x: 400 * scaleX, y: 43 * scaleY, r: 20 * scale })
+	.addKillZone({ id: "south-lava", x: 400 * scaleX, y: 406 * scaleY, r: 20 * scale })
+	.addKillZone({ id: "north-west-corner-lava", x: 72 * scaleX, y: 57 * scaleY, r: 20 * scale })
+	.addKillZone({ id: "north-east-corner-lava", x: 728 * scaleX, y: 57 * scaleY, r: 20 * scale })
+	.addKillZone({ id: "south-west-corner-lava", x: 72 * scaleX, y: 390 * scaleY, r: 20 * scale })
+	.addKillZone({ id: "south-east-corner-lava", x: 728 * scaleX, y: 390 * scaleY, r: 20 * scale })
+	.buildMapDocument();
 }
